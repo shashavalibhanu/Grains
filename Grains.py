79736cb75e9ab5001a5c59dd0b6d5a5726dabb4e
Dudekula Shashavali <120928546+shashavalibhanu@users.noreply.github.com>
2025-01-02 16:54:18 +0530

    Update Grains.py

--- Grains.py
+++ Grains.py
@@ -1,1 +1,14 @@
+def square(number):
+    if number  not in range(1,65):
+        raise ValueError("square must be between 1 and 64")
+    return 2 ** (number-1)
+
+        
+   
+
+
+def total():
+    a =  2 ** 64-1
+    return a
+    pass
 
